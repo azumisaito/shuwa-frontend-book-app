7ce9ff222978354c69728b35a6d4dfb3a01293e1
Azumi S <49363119+sazumy@users.noreply.github.com>
2021-08-03 22:48:06 +0900

    typescriptのインストール

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,24 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  resolve: {
+    extensions: ['.js', '.ts']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)s$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-typescript'
+            ]
+          }
+        }
+      }
+    ]
+  },
 };
